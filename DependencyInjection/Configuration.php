a414b915658b9c7e685e20c8ea6be595c5f3d904
Laurent Sarrazin <mail@laurent-sarrazin.com>
2011-12-07 14:34:43 +0100

    First commit

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Overblog\EmbedlyBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * This is the class that validates and merges configuration from your app/config files
+ *
+ * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html#cookbook-bundles-extension-config-class}
+ */
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('overblog_embedly');
+
+        $rootNode
+            ->children()
+                ->arrayNode('config')
+                    //->ignoreExtraKeys(true)->end()
+                    ->useAttributeAsKey('config') //->prototype('array')
+                        ->children()
+                            ->scalarNode('key')->end()
+                            ->end()
+                        ->end()
+                    //->end()
+                ->end()
+            ->end()
+        ;
+
+        return $treeBuilder;
+    }
+}
---
+++ OverblogEmbedlyBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Overblog\EmbedlyBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class OverblogEmbedlyBundle extends Bundle
+{
+}
